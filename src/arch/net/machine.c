charmplusplus/charm

commit bed4af1a6423921ed52bb06cac281788f97f2523

Bug #962: dynamically allocate inProgress array size
Change-Id: If1cec66d37afe3613e3cd000dbac72a38b01546a

src/arch/net/machine.c
@@ -896,7 +896,7 @@ void printLog(void)
  * SMP implementation moved to machine-smp.c
  *****************************************************************************/
 
-int inProgress[128];
+int* inProgress;
 
 /** Mechanism to prevent dual locking when comm-layer functions, including prints, 
  * are called recursively. (UN)LOCK_IF_AVAILABLE is used before and after a code piece
@@ -2705,6 +2705,8 @@ void ConverseExit(void)
 {
   MACHSTATE(2,"ConverseExit {");
   machine_initiated_shutdown=1;
+  free(inProgress);
+
   if (CmiMyRank()==0) {
     if(Cmi_print_stats)
       printNetStatistics();
@@ -2889,6 +2891,8 @@ void ConverseInit(int argc, char **argv, CmiStartFn fn, int usc, int everReturn)
 
   if (Cmi_charmrun_fd==-1) /*Don't bother with check in standalone mode*/
       Cmi_check_delay=1.0e30;
+
+  inProgress = calloc(_Cmi_mynodesize, sizeof(int));
 
   CsvInitialize(CmiNodeState, NodeState);
   CmiNodeStateInit(&CsvAccess(NodeState));

src/arch/netlrts/machine.c
@@ -796,7 +796,7 @@ static double         Cmi_check_delay = 3.0;
  * OS Threads
  * SMP implementation moved to machine-smp.c
  *****************************************************************************/
-int inProgress[128];
+int* inProgress;
 
 /** Mechanism to prevent dual locking when comm-layer functions, including prints, 
  * are called recursively. (UN)LOCK_IF_AVAILABLE is used before and after a code piece
@@ -1840,6 +1840,7 @@ void LrtsExit()
 {
   int i;
   machine_initiated_shutdown=1;
+  free(inProgress);
 
   CmiStdoutFlush();
   if (Cmi_charmrun_fd==-1) {
@@ -1983,8 +1984,7 @@ void LrtsInit(int *argc, char ***argv, int *numNodes, int *myNodeID)
   if (Cmi_charmrun_fd==-1) /*Don't bother with check in standalone mode*/
       Cmi_check_delay=1.0e30;
 
-  for(i = 0; i < _Cmi_mynodesize; i++)
-    inProgress[i] = 0;
+  inProgress = calloc(_Cmi_mynodesize, sizeof(int));
 
   *numNodes = Lrts_numNodes;
   *myNodeID = Lrts_myNode;

src/arch/verbs/machine.c
@@ -798,7 +798,7 @@ void printLog(void)
 static CmiNodeLock    Cmi_scanf_mutex;
 static double         Cmi_clock;
 static double         Cmi_check_delay = 3.0;
-int inProgress[128];
+int* inProgress;
 
 /** Mechanism to prevent dual locking when comm-layer functions, including prints, 
  * are called recursively. (UN)LOCK_IF_AVAILABLE is used before and after a code piece
@@ -1884,6 +1884,7 @@ void LrtsExit()
 {
   int i;
   machine_initiated_shutdown=1;
+  free(inProgress);
 
   CmiStdoutFlush();
   if (Cmi_charmrun_fd==-1) {
@@ -2015,8 +2016,7 @@ void LrtsInit(int *argc, char ***argv, int *numNodes, int *myNodeID)
   if (Cmi_charmrun_fd==-1) /*Don't bother with check in standalone mode*/
       Cmi_check_delay=1.0e30;
 
-  for(i = 0; i < _Cmi_mynodesize; i++)
-      inProgress[i] = 0;
+  inProgress = calloc(_Cmi_mynodesize, sizeof(int));
 
   *numNodes = Lrts_numNodes;
   *myNodeID = Lrts_myNode;

yektaKamane/Malleable-Jacobi-Solver

commit 5f830cf5cd77efceb161aff6a94a9ca88fd7f2c0

Bug #903: Only free from one thread in smp mode
Fixing the bug that Commit bed4af1a6423921ed52bb06cac281788f97f2523
has introduced

Change-Id: Ief389b80620d3ee4b348764600ab11fc63f15748

src/arch/net/machine.c
@@ -2705,7 +2705,10 @@ void ConverseExit(void)
 {
   MACHSTATE(2,"ConverseExit {");
   machine_initiated_shutdown=1;
-  free(inProgress);
+#if CMK_SMP
+  if(CmiMyRank() == CmiMyNodeSize())
+#endif
+    free(inProgress);
 
   if (CmiMyRank()==0) {
     if(Cmi_print_stats)

src/arch/netlrts/machine.c
@@ -1840,7 +1840,10 @@ void LrtsExit()
 {
   int i;
   machine_initiated_shutdown=1;
-  free(inProgress);
+#if CMK_SMP
+  if(CmiMyRank() == CmiMyNodeSize())
+#endif
+    free(inProgress);
 
   CmiStdoutFlush();
   if (Cmi_charmrun_fd==-1) {

src/arch/verbs/machine.c
@@ -1884,7 +1884,10 @@ void LrtsExit()
 {
   int i;
   machine_initiated_shutdown=1;
-  free(inProgress);
+#if CMK_SMP
+  if(CmiMyRank() == CmiMyNodeSize())
+#endif
+    free(inProgress);
 
   CmiStdoutFlush();
   if (Cmi_charmrun_fd==-1) {